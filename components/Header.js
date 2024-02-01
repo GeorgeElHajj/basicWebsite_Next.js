16bacfa8734bc0eb0326541184842a6a57980fc1
GeorgeElHajj <georgeselhajj46@gmail.com>
2024-02-01 16:26:51 +0200

    using tailwind

--- components/Header.js
+++ components/Header.js
@@ -13,11 +13,11 @@ export default function Header() {
         <header className="fixed top-0	w-full	z-10 bg-bl p-4 text-center	flex justify-between items-center ">
             <img src="/logo.png" alt="Logo" className="w-16 h-auto " />
             <nav className="grow sm:static relative" >
-                <ul className={!burgerBtn ? "text-white items-center justify-center p-0 list-none  sm:flex sm:z-0 sm:bg-transparent sm:flex-row sm:static sm:inset-0 sm:inset-0 "
-                    : "hidden   z-10 	bg-bl flex-col   absolute  top-0   left-0 "}>
-                    <li className="sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#" className="hover:bg-gray-700 px-4 py-2 rounded">Home</a></li>
-                    <li className="sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#services" className="hover:bg-gray-700 px-4 py-2 rounded">Services</a></li>
-                    <li className="sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#contact_us" className="hover:bg-gray-700 px-4 py-2 rounded">Contact</a></li>
+            <ul className={!burgerBtn ? "hidden sm:flex text-white items-center justify-center p-0 list-none sm:static  sm:flex-row sm:inset-0 sm:inset-0"
+                    : "bg-bl absolute top-full left-0 z-10 flex-col w-full sm:w-auto "}>
+                    <li className=" text-white sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#" className="hover:bg-gray-700 px-4 py-2 rounded">Home</a></li>
+                    <li className="text-white sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#services" className="hover:bg-gray-700 px-4 py-2 rounded">Services</a></li>
+                    <li className="text-white sm:mr-4 mr-0 sm:mb-0 mb-4 text-lg	"><a href="#contact_us" className="hover:bg-gray-700 px-4 py-2 rounded">Contact</a></li>
                 </ul>
             </nav>
             <a href="#" className="icon block sm:hidden text-white text-xl mr-4" onClick={handleClick} >
--- components/Services.js
+++ components/Services.js
@@ -1,70 +1,73 @@
 export default function Services() {
     return (
         <div className="flex flex-col items-center m-5 p-5 border border-gray-300 rounded-lg">
-        <h3 className="text-lg font-semibold bl">Services</h3>
-        <div className="flex sm:flex-row flex-col sm:flex-nowrap flex-wrap sm:justify-normal justify-center">
-            <div className="sm:w-1/5 w-1/2  mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border ">
-                <div className="image">
-                    <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md" />
-                </div>
-                <div className="text-center p-2.5">
-                    <h1 className="sm:text-xl text-base font-semibold">Service 1</h1>
-                </div>
-                <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
-                    <p>Description...</p>
-                    <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
-                </div>
-            </div>
-            <div className="sm:w-1/5 w-1/2  mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
-                <div className="image">
-                    <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md" />
-                </div>
-                <div className="text-center p-2.5">
-                    <h1 className="sm:text-xl text-base font-semibold">Service 1</h1>
-                </div>
-                <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
-                    <p>Description...</p>
-                    <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
-                </div>
-            </div>
-            <div className="sm:w-1/5 w-1/2  mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
-                <div className="image">
-                    <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md" />
-                </div>
-                <div className="text-center p-2.5">
-                    <h1 className="sm:text-xl text-base font-semibold">Service 1</h1>
-                </div>
-                <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
-                    <p>Description...</p>
-                    <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
-                </div>
-            </div>
-            <div className="sm:w-1/5 w-1/2  mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
-                <div className="image">
-                    <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md" />
-                </div>
-                <div className="text-center p-2.5">
-                    <h1 className="sm:text-xl text-base font-semibold">Service 1</h1>
-                </div>
-                <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
-                    <p>Description...</p>
-                    <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
-                </div>
-            </div>
-                <div className="sm:w-1/5 w-1/2 	mx-4 m-4 border border-gray-300 rounded-md shadow-lg  shadow-black	box-border	">
+            <h3 className="text-lg font-semibold bl">Services</h3>
+            <div className="grid grid-cols-2 sm:grid-cols-5 gap-4">
+                <div className="mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border ">
                     <div className="image">
-                        <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md " />
+                        <img src="Service.jpg" alt="Service 1" className="w-full rounded-t-md" />
                     </div>
-                    <div className=" text-center p-2.5	">
-                        <h1 className="sm:text-xl text-base	 font-semibold">Service 1</h1>
+                    <div className="text-center p-2.5">
+                        <h1 className="sm:text-xl text-base font-semibold">Service 1</h1>
                     </div>
-                    <div className="p-1 text-center rounded-t-md pt-2.5	text-base	">
+                    <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
                         <p>Description...</p>
-                        <button className="sm:mt-8 mt-3.5  mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5	px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                        <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                    </div>
+                </div>
+
+                <div className="mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
+                    <div className="image">
+                        <img src="Service.jpg" alt="Service 2" className="w-full rounded-t-md" />
+                    </div>
+                    <div className="text-center p-2.5">
+                        <h1 className="sm:text-xl text-base font-semibold">Service 2</h1>
+                    </div>
+                    <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
+                        <p>Description...</p>
+                        <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                    </div>
+                </div>
+
+                <div className="mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
+                    <div className="image">
+                        <img src="Service.jpg" alt="Service 3" className="w-full rounded-t-md" />
+                    </div>
+                    <div className="text-center p-2.5">
+                        <h1 className="sm:text-xl text-base font-semibold">Service 3</h1>
+                    </div>
+                    <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
+                        <p>Description...</p>
+                        <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                    </div>
+                </div>
+
+                <div className="mx-4 m-4 border border-gray-300 rounded-md shadow-lg shadow-black box-border">
+                    <div className="image">
+                        <img src="Service.jpg" alt="Service 4" className="w-full rounded-t-md" />
+                    </div>
+                    <div className="text-center p-2.5">
+                        <h1 className="sm:text-xl text-base font-semibold">Service 4</h1>
+                    </div>
+                    <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
+                        <p>Description...</p>
+                        <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                    </div>
+                </div>
+
+                <div className="mx-4 m-4  border border-gray-300 rounded-md shadow-lg shadow-black box-border">
+                    <div className="image">
+                        <img src="Service.jpg" alt="Service 5" className="w-full rounded-t-md" />
+                    </div>
+                    <div className="text-center p-2.5">
+                        <h1 className="sm:text-xl text-base font-semibold">Service 5</h1>
+                    </div>
+                    <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
+                        <p>Description...</p>
+                        <button className="sm:mt-8 mt-3.5 mb-2 bg-white border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
