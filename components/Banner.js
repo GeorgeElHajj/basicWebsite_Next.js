1566ae37aa44ba2f73d63683c6144da884bbb152
GeorgeElHajj <georgeselhajj46@gmail.com>
2024-02-02 12:14:54 +0200

    final touches

--- components/Banner.js
+++ components/Banner.js
@@ -1,6 +1,6 @@
 export default function Banner() {
     return (
-        <div className="relative overflow-hidden mt-12">
+        <div className="relative overflow-hidden mt-20	">
             <h2 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white z-1 text-4xl">Welcome to My Website</h2>
             <img src="banner.jpg" alt="Banner Image" className="w-full h-auto" />
         </div>
