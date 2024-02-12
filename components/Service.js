92d2e0bc2d905694e3e309a8fdb16938675d76b9
GeorgeElHajj <georgeselhajj46@gmail.com>
2024-02-12 10:09:13 +0200

    swiper design

--- components/Service.js
+++ components/Service.js
@@ -3,14 +3,14 @@ export default function Service({ image, service, description }) {
         <>
             <div className="sm:m-4 m-0 border border-gray-300 rounded-md shadow-lg shadow-black box-border ">
                 <div className="image flex justify-center align-middle">
-                    <img src={image} alt={service} className="w-6/12 rounded-t-md " />
+                    <img src={image} alt={service} className="w-1/4 rounded-t-md " />
                 </div>
                 <div className="text-center p-2.5">
                     <h1 className="sm:text-xl text-base font-semibold">{service}</h1>
                 </div>
                 <div className="p-1 text-center rounded-t-md pt-2.5 text-base">
                     <p>{description}</p>
-                    <button className="sm:mt-8 mt-3.5 mb-2 text-white bg-slate-500	 border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
+                    <button className="sm:mt-6 mt-3.5 mb-6 text-white bg-slate-500	 border border-black rounded-md sm:p-2.5 p-1.5 px-4 py-2 transition-colors duration-500 ease-in-out hover:bg-black hover:text-white hover:cursor-pointer">Read More...</button>
                 </div>
             </div>
 
--- components/Services.js
+++ components/Services.js
@@ -9,6 +9,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+
 export default function Services() {
     return (
         <div className=" m-5 p-5 border border-gray-300 rounded-lg">
@@ -17,14 +18,34 @@ export default function Services() {
                 modules={[Navigation, Pagination, Scrollbar, A11y]}
                 spaceBetween={20}
                 slidesPerView={1}
-                navigation
-                pagination={{ clickable: true }}
-                scrollbar={{ draggable: true }}
+                navigation={{
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                }}
+                pagination={{
+                    el: '.swiper-pagination',
+                    clickable: true,
+                    bulletClass: 'swiper-pagination-bullet',
+                    bulletActiveClass: 'swiper-pagination-bullet-active',
+                }}
+                scrollbar={{
+                    el: '.swiper-scrollbar',
+                    draggable: true,
+                }}
             >
                 <div className="grid grid-cols-2 sm:grid-cols-5 gap-4">
-                {Data.map((service) =>  <SwiperSlide> <Service key={service.service} {...service} /></SwiperSlide>)}  
+                    {Data.map((service) => <SwiperSlide> <Service key={service.service} {...service} /></SwiperSlide>)}
                 </div>
+                <div className="swiper-button-next absolute top-1/2 transform -translate-y-1/2 right-10 z-10 bg-bl p-6 rounded-full shadow-md cursor-pointer"></div>
+                <div className="swiper-button-prev absolute top-1/2 transform -translate-y-1/2 left-10 z-10 bg-bl p-6 rounded-full shadow-md cursor-pointer"></div>
+                <div className="swiper-pagination absolute bottom-5 left-1/2 transform -translate-x-1/2 text-center bg-gray-400 rounded-full text-sm px-2 py-1">
+                    <span className="swiper-pagination-bullet bg-black rounded-full w-6 h-6 inline-block m-1"></span>
+                    <span className="swiper-pagination-bullet bg-black rounded-full w-6 h-6 inline-block m-1"></span>
+                    <span className="swiper-pagination-bullet bg-black rounded-full w-6 h-6 inline-block m-1"></span>
+                </div>
+                <div className="swiper-scrollbar absolute bottom-0 w-full bg-gray-200 rounded-full"></div>
             </Swiper>
+
         </div>
     );
 }
