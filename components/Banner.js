5cca88328f415507d37ab6ff65386a85bbb1c688
GeorgeElHajj <georgeselhajj46@gmail.com>
2024-02-13 14:25:47 +0200

    swiper/accordion

--- components/Banner.js
+++ components/Banner.js
@@ -1,8 +1,8 @@
 export default function Banner({text,image}) {
     return (
-        <div className="mx-auto relative mt-20 min-h-full">
+        <div className="mx-auto relative mt-20 ">
             <h2 className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 text-white z-1 text-4xl text-center">{text}</h2>
-            <img src={image} alt="Banner Image" className="w-full h-auto" />
+            <img src={image} alt="Banner Image" className="w-full h-auto min-h-64 object-cover" />
         </div>
     );
 }
